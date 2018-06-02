37de0f5a4486b2868233165061cb76ec22d35c94
Mario Orlandi <morlandi@brainstorm.it>
2018-06-02 18:30:35 +0200

    Trace time elapsed for each download; also, detect longest one

--- news_collector/collector/views.py
+++ news_collector/collector/views.py
@@ -1,8 +1,9 @@
+import datetime
+import requests
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.views.generic import View
 from django.http import JsonResponse
-import requests
 from .constants import BLOGS
 
 
@@ -14,13 +15,22 @@ def index(request):
 def news_collector_sync_view(request):
 
     data = {}
+    t0 = datetime.datetime.now()
+    max_dt = 0
+
     for name, link in BLOGS.items():
-        print('Downloading "%s" from "%s" ...' % (name, link))
+        t1 = datetime.datetime.now()
         response = requests.get(link)
         if response.status_code != 200:
             data[name] = 'Download error'
         else:
             data[name] = response.content.decode("utf-8")
-    print('Download completed')
+        dt = (datetime.datetime.now() - t1).total_seconds()
+        print(f'Downloaded "{name}" from "{link}" in {dt} [s]')
+        max_dt = max(dt, max_dt)
+
+    dt = (datetime.datetime.now() - t0).total_seconds()
+    print(f'All downloads completed; elapsed time: {dt} [s]')
+    print(f'Slowest download required: {max_dt} [s]')
 
     return JsonResponse(data)
