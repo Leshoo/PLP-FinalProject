0bdf401845f626faac1088c990afae5abf6ddcfe
Mario Orlandi <morlandi@brainstorm.it>
2018-06-02 18:31:19 +0200

    inline fetch loop

--- news_collector/collector/consumers.py
+++ news_collector/collector/consumers.py
@@ -1,6 +1,7 @@
 import asyncio
 import requests
 import json
+import datetime
 from aiohttp import ClientSession
 from django.conf import settings
 from channels.generic.http import AsyncHttpConsumer
@@ -14,35 +15,28 @@ class NewsCollectorAsyncConsumer(AsyncHttpConsumer):
         # Adapted from:
         # "Making 1 million requests with python-aiohttp"
         # https://pawelmhm.github.io/asyncio/python/aiohttp/2016/04/22/asyncio-aiohttp.html
-
         async def fetch(url, session):
             async with session.get(url) as response:
                 return await response.read()
 
-        async def run(blogs):
-            tasks = []
+        tasks = []
 
-            # Fetch all responses within one Client session,
-            # keep connection alive for all requests.
-            async with ClientSession() as session:
-                for name, url in blogs.items():
-                    print('Downloading "%s"' % name)
-                    task = asyncio.ensure_future(fetch(url, session))
-                    tasks.append(task)
+        # Fetch all responses within one Client session,
+        # keep connection alive for all requests.
+        t0 = datetime.datetime.now()
+        async with ClientSession() as session:
+            for name, url in BLOGS.items():
+                print('Start downloading "%s"' % name)
+                task = asyncio.ensure_future(fetch(url, session))
+                tasks.append(task)
 
-                responses = await asyncio.gather(*tasks)
-                # you now have all response bodies in this variable
-                #print(responses)
+            # gather all responses
+            responses = await asyncio.gather(*tasks)
+            dt = (datetime.datetime.now() - t0).total_seconds()
+            print(f'All downloads completed; elapsed time: {dt} [s]')
 
-            return responses
-
-        # loop = asyncio.get_event_loop()
-        # future = asyncio.ensure_future(run(BLOGS))
-        # loop.run_until_complete(future)
-
-        responses = await run(BLOGS)
-
-        #data = dict([('???', r.decode('utf-8')) for r in responses])
+        # Here, we assume that the responses have been gathered
+        # in the same order as the original task list. Is this correct ?
         data = dict(zip(BLOGS.keys(), [r.decode('utf-8') for r in responses]))
         text = json.dumps(data)
 
