ee557bbea6ef74069a57302af70db0c53319db3b
Godfrey Leshoo <92622095+Leshoo@users.noreply.github.com>
2025-01-13 14:40:19 +0300

    Update view.py

--- news_collector/news_collector/view.py
+++ news_collector/news_collector/view.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from .forms import UserRegistrationForm
 
-def chat_room(request, room_name):
-    return render(request, 'chat/chat_room.html', {
-        'room_name': room_name
-    })
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            return redirect('login')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('chat_room', room_name='default')
+    return render(request, 'registration/login.html')
