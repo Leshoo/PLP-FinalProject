1b2fa2243f7b76889d9232beb3a0f669e0c1e097
Godfrey Leshoo <92622095+Leshoo@users.noreply.github.com>
2025-01-13 14:49:36 +0300

    Update urls.py

--- news_collector/news_collector/urls.py
+++ news_collector/news_collector/urls.py
@@ -4,3 +4,8 @@ from . import views
 urlpatterns = [
     path('chat/<str:room_name>/', views.chat_room, name='chat_room'),
 ]
+urlpatterns = [
+    ...
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+]
